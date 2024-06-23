eb7ffb4b4f64c677fbe1fde5cc5e7543b22d630c
Anny <utp0153537@alumno.utpuebla.edu.mx>
2024-06-23 16:18:55 -0600

    Cuarto Commit: Agregación de img en Main

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Main.css';
+import imagenIndex from './components/img_index_A.png';
 
 const Main = () => {
   return (
     <main className="main-content">
       <section className="hero">
+      <img src={imagenIndex} alt="" className="centered-image" />
         <h1>TRABAJADORES A TU ALCANCE</h1>
         <h2>CONECTANDO PERSONAS</h2>
         <p>
